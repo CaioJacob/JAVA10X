9b0c1a1b1d1eaab8c2e6d2185601e2261a83d2b6
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-05-30 12:52:20 +0100

    Update PrimitiveData.java

--- JavaNinja/src/DataTypes/PrimitiveData.java
+++ JavaNinja/src/DataTypes/PrimitiveData.java
@@ -8,11 +8,16 @@ public class PrimitiveData {
         * Objective of the class: Create a ninja - Naruto -
         * */
 
-        int age =16; // Max value: 2,147,483,647
+        int age =17; // Max value: 2,147,483,647
         double height = 1.65;
         char initial = 'N';
         boolean aliveOrDead = true;
         Long bankBalance = 9999999L; // Max value:9,223,372,036,854,775,807
+
+        System.out.println(age); // Command to show to user
+        System.out.println(height);
+        System.out.println("Bank Balance is = " + bankBalance);
+        System.out.println("My age is: " + age);
 
     }
 
