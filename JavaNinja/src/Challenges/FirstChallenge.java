f6b1599e25d375610d274d3133025b6500a9ea6e
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-05-30 13:52:52 +0100

    First Challenge

---
+++ JavaNinja/src/Challenges/FirstChallenge.java
@@ -0,0 +1,34 @@
+package Challenges;
+
+public class FirstChallenge {
+    public static void main(String[] args) {
+
+        //Creating Naruto
+        String nameNinja1 = "Naruto Uzumaki";
+        int ageNinja1 = 16;
+        String missionNinja1 = "Help at the Ramen stand";
+        String difficultyLevelNinja1 = "Easy";
+        String statusNinja1 = "Completed";
+
+        System.out.println(nameNinja1 + " at " + ageNinja1 + " years old received the mission of " + missionNinja1 + " of difficulty " + difficultyLevelNinja1 + " and its status is: " + statusNinja1 + ".");
+
+        //Creating Sasuke
+        String nameNinja2 = "Sasuke Uchiha";
+        int ageNinja2 = 17;
+        String missionNinja2 = "Hunt some bandits";
+        String difficultyLevelNinja2 = "Moderate";
+        String statusNinja2 = "In progress";
+
+        System.out.println(nameNinja2 + " at " + ageNinja2 + " years old received the mission of " + missionNinja2 + " of difficulty " + difficultyLevelNinja2 + " and its status is: " + statusNinja2 + ".");
+
+        //Creating Sakura
+        String nameNinja3 = "Sakura Haruno";
+        int ageNinja3 = 15;
+        String missionNinja3 = "Help in the hospital";
+        String difficultyLevelNinja3 = "Easy";
+        String statusNinja3 = "Completed";
+
+        System.out.println(nameNinja3 + " at " + ageNinja3 + " years old received the mission of " + missionNinja3 + " of difficulty " + difficultyLevelNinja3 + " and its status is: " + statusNinja3 + ".");
+
+    }
+}
