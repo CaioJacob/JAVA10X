15d3969ff96b3bd53ee04cd059188a6a5f3cb90b
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-05-30 12:37:47 +0100

    PrimitiveData
    1st class.

---
+++ JavaNinja/src/DataTypes/PrimitiveData.java
@@ -0,0 +1,21 @@
+package DataTypes;
+
+public class PrimitiveData {
+    public static void main(String[] args) {
+
+        /*
+        * Primitive data - int, double, float, char, boolean, short.
+        * Objective of the class: Create a ninja - Naruto -
+        * */
+
+        int age =16; // Max value: 2,147,483,647
+        double height = 1.65;
+        char initial = 'N';
+        boolean aliveOrDead = true;
+        Long bankBalance = 9999999L; // Max value:9,223,372,036,854,775,807
+
+
+
+    }
+
+}
