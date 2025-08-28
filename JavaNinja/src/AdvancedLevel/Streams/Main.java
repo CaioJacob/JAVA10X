6eb927c920bc6ece3a3f5c27e5994ab9cf19c076
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-08-28 10:41:14 +0100

    Stream Part 2

--- JavaNinja/src/AdvancedLevel/Streams/Main.java
+++ JavaNinja/src/AdvancedLevel/Streams/Main.java
@@ -11,7 +11,7 @@ public class Main {
         ninjas.add(new Ninja("Naruto Uzumaki", "Konohagakure", 14));
         ninjas.add(new Ninja("Sasuke Uchiha", "Konohagakure", 40));
         ninjas.add(new Ninja("Sakura Haruno", "Konohagakure", 9));
-        ninjas.add(new Ninja("Kakashi Hatake", "Konohagakure", 30));
+        ninjas.add(new Ninja("Kakashi Hatake", "Konohagakure", 67));
         ninjas.add(new Ninja("Gaara", "Sunahagakure", 28));
         ninjas.add(new Ninja("Temari", "Sunahagakure", 39));
         ninjas.add(new Ninja("Rock Lee", "Konohagakure", 57));
@@ -25,10 +25,31 @@ public class Main {
 
         // Ordination
         ninjas.stream()
-                .sorted((n1, n2) -> CharSequence.compare(n1.getName(), n2.getName()))
+                .sorted((n1, n2) -> n1.getName().compareTo(n2.getName()))
                 .forEach(System.out::println);
 
+        System.out.println("======================================");
 
+        // Map - Show and map an attribute
+
+        ninjas.stream()
+                .map(Ninja::getName)
+                .forEach(System.out::println);
+
+        System.out.println("======================================");
+
+        ninjas.stream()
+                .map(Ninja::getAge)
+                .forEach(System.out::println);
+
+        System.out.println("======================================");
+
+        // MAX - Filter by older ninja
+        Ninja eldestNinja = ninjas.stream()
+                .max((n1, n2) -> Integer.compare(n1.getAge(), n2.getAge()))
+                .orElse(null);
+
+        System.out.println("Ninja filtered by the eldest: " + eldestNinja);
 
 
     }
